9530229ab19dbd3df55c1e059484409146d022ee
Anthony Konschuh <silv.amee@gmail.com>
2017-07-10 15:34:43 -0700

    Sets up basic server
    Sets up express server and port listner

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {root: './public'})
+});
+
+app.listen(PORT, () => console.log(`You are on port: ${PORT}. Enjoy the storm.`));
